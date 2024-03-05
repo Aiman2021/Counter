f1f789852c1802e3bd7df6add5920e6b8af78961
Aiman <Aimaha80@mail.ru>
2024-03-05 22:35:13 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({ count: 0 });
+  const increment = () => setState({ count: state.count + 1 });
+  const decrement = () => setState({ count: state.count - 1 });
+  const reset = () => setState({ count: 0 });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Counter App: {state.count}</h1>
+      <button onClick={increment} disabled={state.count===10 ? true : false}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>RESET</button>
     </div>
   );
 }
 
 export default App;
+
+/* 
+1.вместо объекта храним просто число
+2. В кнопке button задействуем свойство disabled 
+3. функцию сделать одной строкой */
